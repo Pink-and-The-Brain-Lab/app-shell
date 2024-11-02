e7b951f398593a52df91bb05c8f59f2590cf43b2
André Lagacione <151460799+lagacioneandre@users.noreply.github.com>
2024-11-02 18:20:50 -0300

    .

--- src/app/shared/authguard.guard.ts
+++ src/app/shared/authguard.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, CanActivateChild, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthdGuard implements CanActivate, CanActivateChild {
+export class AuthdGuard  {
 
   constructor(
     private authService: AuthService,
